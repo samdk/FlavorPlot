057ac2938e0b54bb4633948effbb8f164f7faf76
RJ Marsan <robert.marsan@hulu.com>
2010-07-24 02:46:49 -0700

    spider is working. cool

--- ingest/spider.rb
+++ ingest/spider.rb
@@ -1,2 +1,7 @@
 class Spider
+	def spider
+		while next? 
+			yield next_recipe
+		end 
+	end
 end
--- ingest/spiders/allrecipes.rb
+++ ingest/spiders/allrecipes.rb
@@ -2,6 +2,12 @@ class AllRecipesSpider < Spider
   PRINT_URL              = "http://allrecipes.com/Recipe-Tools/Print/Recipe.aspx?origin=detail&servings=16&RecipeID="
   RECIPE_URL             = "http://allrecipes.com/"
   BACK_TO_ORIGINAL_CLASS = "ctl00_CenterColumnPlaceHolder_printPage_lnkBackToRecipe"
+	
+	def initialize
+		@start = 7000
+		@finish = 7015
+		@current = @start
+	end
 
 	def recipe_link(html)
 		$1 if html.scan /<a.*id=\"ctl00_CenterColumnPlaceHolder_printPage_lnkBackToRecipe\".*href="(.*)"/
@@ -16,4 +22,14 @@ class AllRecipesSpider < Spider
 		recipe_url = make_recipe_url(recipe_link(response))
 		Net::HTTP.get URI.parse(recipe_url)
 	end
+
+	def next?
+		@current < @finish
+	end
+	
+	def next_recipe
+		@current+=1
+		@current+=1 until ((result = parse(@current)) || ! next?)
+		result
+	end 
 end
