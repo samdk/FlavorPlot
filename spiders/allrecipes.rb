a0fe1f7c1f4fb4cf853d67003d74c369c3eba466
RJ Marsan <robert.marsan@hulu.com>
2010-07-24 00:45:26 -0700

    classifuying

--- spiders/allrecipes.rb
+++ spiders/allrecipes.rb
@@ -6,22 +6,23 @@ PRINT_URL = "http://allrecipes.com/Recipe-Tools/Print/Recipe.aspx?origin=detail&
 RECIPE_URL = "http://allrecipes.com/"
 BACK_TO_ORIGINAL_CLASS = "ctl00_CenterColumnPlaceHolder_printPage_lnkBackToRecipe"
 
+class AllRecipeSpider
 
-def lookForRecipeLink(html)
-	$1 if html.scan(/<a.+?id=\"ctl00_CenterColumnPlaceHolder_printPage_lnkBackToRecipe\".+?href="(.+?)"/)
+	def lookForRecipeLink(html)
+		$1 if html.scan(/<a.+?id=\"ctl00_CenterColumnPlaceHolder_printPage_lnkBackToRecipe\".+?href="(.+?)"/)
+	end
+
+	def make_recipe_url(link) 
+		RECIPE_URL + link[6..-1]
+	end
+
+	def parse(id)
+		response = Net::HTTP.get URI.parse(PRINT_URL+id.to_s)
+		link = lookForRecipeLink(response)
+		recipeurl = make_recipe_url link
+		puts recipeurl
+		recipe_response = Net::HTTP.get URI.parse(recipeurl)
+	end
+
 end
-
-def make_recipe_url(link) 
-	RECIPE_URL + link[6..-1]
-end
-
-def parse(id)
-	response = Net::HTTP.get URI.parse(PRINT_URL+id.to_s)
-	link = lookForRecipeLink(response)
-	recipeurl = make_recipe_url link
-	puts recipeurl
-	recipe_response = Net::HTTP.get URI.parse(recipeurl)
-end
-
-
 #parse(nil, 17706, 17713)
