7fff22c63c15a696a6cf879c1bf3a665d6857f35
Eli <eli@elilies.com>
2010-07-24 21:04:57 -0700

    Add some bad words

--- ingest/parser.rb
+++ ingest/parser.rb
@@ -2,18 +2,20 @@ class Parser
   
   BAD_WORDS = %w[ tbsp tbsp t tablespoon tablespoon tsp teaspoon
                   cup pint quart liter gallon ml
-                  pinch can jar package
+                  pinch can jar package taste
                   oz ounce lb pound fl
-                  optional to cover
+                  optional to cover for garnish
+                  whole chunk sliced diced chopped flaked package minced thinly corsely
+                    finely slice beaten dried dry piece sifted cooked
                   degrees
                 ]
   
   def self.extract_ingredient(ing)
     ingredient = []
-    ing = ing.downcase.gsub(/\(.*\)/,'')
+    ing = ing.downcase.gsub(/\(.*\)/,'').gsub(/[^a-z]/,'')
     ing.split.each do |word|
       word = word.singularize
-      if BAD_WORDS.include?(word.gsub(/\W/,'')) || word =~ /\d/ || word.size <= 2
+      if BAD_WORDS.include?(word) || word =~ /\d/ || word.size <= 2
         ingredient = []
       else
         ingredient << word
